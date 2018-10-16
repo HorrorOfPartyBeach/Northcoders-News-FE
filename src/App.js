628d92583feafc9b5c3ec926a725193805c23934
Emma Gilmour <e.gilmour33@gmail.com>
2018-10-16 10:28:48 +0100

    hardcoded some data to test layout

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1>Header</h1>
+          <header><h1>Header</h1></header>
+          <nav>Home Topics Users</nav>
+          
+          <section>
+            <h2>The Rise Of Thinking Machines: How IBM's Watson Takes On The World</h2>
+            <p>
+            Many people know Watson as the IBM-developed cognitive super computer that won the Jeopardy! gameshow in 2011. In truth, Watson is not actually a computer but a set of algorithms and APIs, and since winning TV fame (and a $1 million prize) IBM has put it to use tackling tough problems in every industry from healthcare to finance. Most recently, IBM has announced several new partnerships which aim to take things even further, and put its cognitive capabilities to use solving a whole new range of problems around the world.
+            </p>
+            </section>
+          
+          <footer>Footer</footer>
       </div>
     );
   }
