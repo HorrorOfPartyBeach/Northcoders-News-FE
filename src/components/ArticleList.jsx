888052e3a9ffc7b9c4c905ecdd73ab43f313ff60
Emma Gilmour <e.gilmour33@gmail.com>
2018-10-17 09:50:10 +0100

    fixed get articles

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import * as api from '../Api.js';
+import Voter from '../components/Voter';
 
 class ArticleList extends Component {
     state = {
@@ -6,17 +8,40 @@ class ArticleList extends Component {
     }
 
     render() {
+        console.log(this.props);
        return (
            <main>
             <p>Articles</p>
-            {this.state.articles.map(({_id, title}) => {
+            {this.state.articles.map(({_id, title, belongs_to}) => {
                  return <div key={_id}>
-                <h2 key={_id}>{title}></h2>
+                <h2 key={_id}>{title}</h2>
+                <Voter id={_id}/>
+                <p>{belongs_to}</p>
                 </div>
             })}
            </main>
        )
     }
+    componentDidMount() {
+        console.log(this.props.topic)
+        this.fetchArticles();
+    }
+
+    componentDidUpdate (prevProps) {
+        // console.log(this.props)
+        if(prevProps.topic !== this.props.topic) {
+            this.fetchArticles();
+        }
+    }
+
+    fetchArticles = () => {
+        api.getArticles().then(articles => {
+            this.setState({
+                articles
+            })
+        })
+    }
+
 }
 
 export default ArticleList;
