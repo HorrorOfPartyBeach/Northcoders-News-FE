c9ad0584fdcfd579fdd2a696d021cdcc62d13eaa
Emma Gilmour <e.gilmour33@gmail.com>
2018-10-17 09:49:12 +0100

    add articles by topic route

--- src/App.js
+++ src/App.js
@@ -12,22 +12,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <header><h1>Header</h1></header>
+          <header><h1>Northcoders News</h1></header>
           <Nav />
           
         <Router className="main">
           <ArticleList path='/'/>
           <ArticleList path='/articles'/>
+          <ArticleList path='/topics/:topic'/>
         </Router>
-
-          {/* <section>
-            <h2>The Rise Of Thinking Machines: How IBM's Watson Takes On The World</h2>
-            <p>
-            Many people know Watson as the IBM-developed cognitive super computer that won the Jeopardy! gameshow in 2011. In truth, Watson is not actually a computer but a set of algorithms and APIs, and since winning TV fame (and a $1 million prize) IBM has put it to use tackling tough problems in every industry from healthcare to finance. Most recently, IBM has announced several new partnerships which aim to take things even further, and put its cognitive capabilities to use solving a whole new range of problems around the world.
-            </p>
-            </section> */}
-          
-          <footer>Footer</footer>
+  
+          <footer>@HorrorOfPartyBeach</footer>
       </div>
     );
   }
